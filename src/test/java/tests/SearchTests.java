bfae7eadafc1f735e6d9a93af899f6b867d7da09
Conjuction <suhinin-d@mail.ru>
2023-03-22 18:41:48 +0500

    add tests

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -12,6 +12,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
 import static org.openqa.selenium.By.id;
 
 public class SearchTests extends TestBase {
@@ -27,7 +28,28 @@ public class SearchTests extends TestBase {
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
     }
+    @Tag("ios")
+    @DisplayName("Проверка на вывод текста")
+    @Test
+    void outputTextTest() {
 
+        String text = "Hello";
+
+        step("Click text button", () ->
+                $(id("Text Button")).click());
+
+        step("Check initial state for output text", () ->
+                $(id("Text Output")).shouldHave(text("Waiting for text input")));
+
+        step(format("Set value %s in input and press enter", text), () -> {
+            $(id("Text Input")).click();
+            $(id("Text Input")).sendKeys(text);
+            $(id("Text Input")).pressEnter();
+        });
+
+        step("Verify output text", () ->
+                $(id("Text Output")).shouldHave(text(text)));
+    }
 
     @DisplayName("Checking text in due order")
     @Test
