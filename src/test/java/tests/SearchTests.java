824e001ba5e25c7c914249735c8fe474f7b1a0ae
Conjuction <suhinin-d@mail.ru>
2023-03-23 23:19:38 +0500

    add tests

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,18 +1,15 @@
 package tests;
 
-import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.qameta.allure.Allure.step;
-import static java.lang.String.format;
 import static org.openqa.selenium.By.id;
 
 public class SearchTests extends TestBase {
@@ -32,7 +29,7 @@ public class SearchTests extends TestBase {
     @DisplayName("Checking text in due order")
     @Test
     @Tag("android")
-    void CheckButtonLogIn() {
+    void checkButtonLogIn() {
 
         step("Click on the NavBar Menu button", () -> {
             $(id("org.wikipedia.alpha:id/menu_overflow_button")).click();
